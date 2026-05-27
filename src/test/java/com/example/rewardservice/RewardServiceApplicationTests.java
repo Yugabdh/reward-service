8f9602bc5d53263ef9b323055002d88d3ff110ca
Yugabdh <yugabdhppashte@gmail.com>
2026-05-27 20:24:11 +0530

    first commit

---
+++ src/test/java/com/example/rewardservice/RewardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rewardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
